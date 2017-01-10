41770af70871ffab70033a8c531811d90eacc00f
Ramon Pereira <ramon@alunos.utfpr.edu.br>
2017-01-10 13:40:54 -0200

    Fixed Type Treatment and Form
    -Moved images to resources folder
    
    Change-Id: I091fa204e082f5092169c7210935ec53472f9eca

--- server.js
+++ server.js
@@ -4,40 +4,37 @@ var url = require('url');
 var path = require('path');
 
 var contentTypes = {
-		  'html' : 'text/html',
-		  'css'  : 'text/css',
-		  'ico'  : 'image/x-icon',
-		  'png'  : 'image/png',
-		  'svg'  : 'image/svg+xml',
-		  'js'   : 'application/javascript',
-		  'jpg'	 : 'image/jpg',		
-		  'otf'  : 'application/x-font-otf',
-		  'ttf'  : 'application/x-font-ttf',
-		  'eot'  : 'application/vnd.ms-fontobject',
-		  'woff' : 'application/x-font-woff',
-		  'woff2': 'application/font-woff2',
-		  'zip'  : 'application/zip'
+          'html' : 'text/html;charset=UTF-8',
+          'css'  : 'text/css',
+          'ico'  : 'image/x-icon',
+          'png'  : 'image/png',
+          'svg'  : 'image/svg+xml',
+          'js'   : 'application/javascript',
+          'jpg'  : 'image/jpg',
+          'otf'  : 'application/x-font-otf',
+          'ttf'  : 'application/x-font-ttf',
+          'eot'  : 'application/vnd.ms-fontobject',
+          'woff' : 'application/x-font-woff',
+          'woff2': 'application/font-woff2',
+          'zip'  : 'application/zip'
 };
 
 // Create a server
-http.createServer( function (request, response) {  
+http.createServer(function (request, response) {
    // Parse the request containing file name
    var pathName = url.parse(request.url).pathname;
-   
+
    // Print the name of the file for which request is made.
    console.log("Request for " + pathName + " received.");
-   
+
    // Read the requested file content from file system
    if (pathName.substr(1) == "")
    {
-	   pathName="/login.html";
+       pathName="/login.html";
    }
-   
+
    var fileName = pathName.substr(1);
 
-   //console.log("filename: " + fileName);
-   //console.log("pathName: " + pathName);
-   
    fs.readFile(pathName.substr(1), function (err, data) {
       if (err) {
          console.log(err);
@@ -45,21 +42,20 @@ http.createServer( function (request, response) {
          // HTTP Status: 404 : NOT FOUND
          // Content Type: text/plain
          //response.writeHead(404, {'Content-Type': 'text/html'});
-         response.writeHead(404, {"Content-Type": "text/plain"});
-         response.write("404 Not Found\n");
+         response.writeHead(404, {'Content-Type': 'text/plain'});
+         response.write("404 Page Not Found\n");
          response.end();
-      }else {	
-         //Page found	  
+      } else {
+         //Page found
          // HTTP Status: 200 : OK
          // Content Type: text/plain
-    	 var extension = path.extname(pathName).substr(1);
-    	 console.log(extension)
+         var extension = path.extname(pathName).substr(1);
          response.writeHead(200, {'Content-Type': contentTypes[extension]});
-         
+
          // Write the content of the file to response body
-         response.write(data.toString());		
+         response.write(data);
       }
-      // Send the response body 
+      // Send the response body
       response.end();
    });
 }).listen(8081);
