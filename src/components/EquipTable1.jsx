c459634f5897a6e2a17721d3f3e5ad9699be23f5
Ramon Felipe Pereira <ramon@alunos.utfpr.edu.br>
2017-03-12 22:16:34 -0300

    Sortby Table with material ui
    Change-Id: I988272b43df3d1bf823f7e0768286bf97a50f425

---
+++ src/components/EquipTable1.jsx
@@ -0,0 +1,141 @@
+import React from 'react';
+import axios from 'axios';
+import Field from './Field.jsx';
+import {Table, TableBody, TableFooter, TableHeader, TableHeaderColumn, TableRow, TableRowColumn} from 'material-ui/Table';
+import RaisedButton from 'material-ui/RaisedButton';
+import { MuiThemeProvider} from 'material-ui';
+import update from 'immutability-helper';
+
+let aux = [];
+
+let headerProps = {
+  enableSelectAll: false,
+  displaySelectAll: false,
+  adjustForCheckbox: false
+};
+
+let headers = [
+  {name: "Código de Barras", key: "eq_id"},
+  {name: "Patrimônio", key: "pat"},
+  {name: "Nome", key: "type"},
+  {name: "Estado", key: "state"},
+  {name: "Data", key: "date"},
+  {name: "Hora", key: "time"}
+  
+];
+
+export default class EquipTable extends React.Component {
+	constructor(props) {
+		super(props);
+    this.state = {
+      data: [], sortBy: 'state'
+    };
+  }
+  
+  loadDataFromServer() {
+		axios.get(this.props.url)
+			.then((response) => {
+        this.setState(update(this.state, {data: {$set: response.data}}));
+			})
+			.catch((error) => {
+        if(error.response)
+          this.setState(update(this.state, {data: {$set: error.response.data}}));
+        else
+          this.setState(update(this.state, {
+            data: {0: {state: {$set: error.toString()}}}
+          }));
+			});
+  }
+  
+  renderHeaders(){
+    let header= headers.map( (h) => {
+      return <SortableHeader 
+                name={h.name}
+                key={h.key}
+                onClicked={()=>this.updateSortBy(h.key)} 
+                isSortColumn={this.state.sortBy == h.key}/>
+    });
+    return <TableRow>{header}</TableRow>;
+  }
+
+   updateSortBy(sortBy){
+      // multiple clicks on the same column reverse the sort order
+      if( sortBy == this.state.sortBy ){
+        this.setState( {data: [...this.state.data.reverse()]} );
+        return;
+      }
+      
+      let data = [...this.state.data];
+      data.sort( (a,b) => {
+        if (a[sortBy] < b[sortBy])
+          return -1;
+        if(a[sortBy] > b[sortBy])
+          return 1;
+        return 0;
+      });
+      
+      this.setState({data, sortBy});
+    }
+
+
+  render () {
+  
+    return (
+    <MuiThemeProvider>
+      <div>
+        <Table
+          height='300px'
+          fixedHeader={true}
+          selectable={false}
+          multiSelectable={false}
+        >
+          <TableHeader {...headerProps}>
+              {this.renderHeaders()}
+          </TableHeader>
+          
+          <TableBody
+            displayRowCheckbox={false}
+            showRowHover={true}
+            stripedRows={true}
+          >
+            {this.state.data.map( (row, i) => (
+                <TableRow key={row.eq_id}>
+                    <TableRowColumn>{row.eq_id}</TableRowColumn>
+                    <TableRowColumn>{row.pat}</TableRowColumn>
+                    <TableRowColumn>{row.type}</TableRowColumn>
+                    <TableRowColumn>{row.state}</TableRowColumn>
+                    <TableRowColumn>{row.date}</TableRowColumn>
+                    <TableRowColumn>{row.time}</TableRowColumn>
+                </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+        <RaisedButton label="Recarregar" primary={true} onClick={this.loadDataFromServer.bind(this)}/>
+        
+      </div>
+      </MuiThemeProvider>
+    );
+  }
+}
+
+function SortableHeader(props){
+  let style = {
+    cursor: "pointer"
+  }
+  if(props.isSortColumn){
+    style.fontWeight = "bold";
+    style.color = "black";
+  }
+  
+  return (
+    <TableHeaderColumn>
+      <div style={style} onClick={() => props.onClicked()}>{props.name}</div>
+    </TableHeaderColumn>
+  );
+}
+  
+
+
+EquipTable.propTypes = {
+  url: React.PropTypes.string.isRequired
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
+import App1 from './components/App1.jsx';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-main();
+main1();
 
 function main() {
   injectTapEventPlugin();
@@ -15,4 +16,31 @@ function main() {
       <App />
     </MuiThemeProvider>
     , app);
+}
+
+function main1() {
+  const aux = 1;
+  if(aux === 1)
+  {
+  injectTapEventPlugin();
+  const app1 = document.createElement('div');
+  document.body.appendChild(app1);
+  ReactDOM.render(
+    <MuiThemeProvider>
+      <App1 />
+    </MuiThemeProvider>
+    , app1);
+    }
+  
+  else
+  {
+  injectTapEventPlugin();
+  const app = document.createElement('div');
+  document.body.appendChild(app);
+  ReactDOM.render(
+    <MuiThemeProvider>
+      <App />
+    </MuiThemeProvider>
+    , app);
+    }
 }
