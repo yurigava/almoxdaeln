c9929566a9e41b1c5072698c4eec480479104ec5
Ramon Felipe Pereira <ramon@alunos.utfpr.edu.br>
2017-03-26 20:11:36 -0300

    Added Passport Authentication
    Change-Id: Ie3d293643f5ef900ff9195d5d3a52f9d7140ee4e

---
+++ config/database.js
@@ -0,0 +1,10 @@
+// config/database.js
+module.exports = {
+    'connection': {
+        'host': '127.0.0.1',
+        'user': 'jquery',
+        'password': 'Test123!.'
+    },
+	'database': 'authentication_db',
+        'users_table': 'users'
+};
--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 var fs = require('fs');
 var url = require('url');
 var path = require('path');
+var morgan = require('morgan');
 var express = require('express');
+var passport = require('passport');
+//var flash    = require('connect-flash');
+var bodyParser = require('body-parser');
+var session  = require('express-session');
+var cookieParser = require('cookie-parser');
+var LocalStrategy = require('passport-local').Strategy;
 var app = express();
 
 var router = express.Router();
@@ -21,6 +28,34 @@ connection.connect(function (err) {
     }
 });
 
+require('./config/passport')(passport); // pass passport for configuration
+
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser.json());
+
+var expiryDate = new Date( Date.now() + 60 * 60 * 1000 ); // 1 hour
+
+// required for passport
+app.use(session({
+	secret: 'vidyapathaisalwaysrunning',
+	resave: true,
+	saveUninitialized: true,
+	cookie: {
+            //secure: true, // Assegura que o navegador só envie o cookie por HTTPS.
+            //httpOnly: true, //Assegura que o cookie seja enviado apenas por HTTP(S), não por cliente JavaScript, ajudando assim a se proteger contra ataques de cross-site scripting.
+            expires: expiryDate
+        }
+ } )); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+
+// routes ======================================================================
+//require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+
+require('./src/authentication.js')(app, passport);
+
 app.set('port', (process.env.PORT || 8081));
 //Requests to / to public
 app.use('/', express.static('public'));
@@ -36,10 +71,12 @@ app.disable('etag');
 app.get('/api/equips', function(req, res) {
   connection.query('SELECT * FROM almoxdaeln_db.Equipments', function (error, results, fields) {
     if (error) throw error;
-		console.log('Received request on /api/equips')
+    console.log('Received request on /api/equips')
     res.send(results);
   });
 });
+
+//app.post('/login', function(req,res) {}
 
 app.listen(app.get('port'), function() {
   console.log('Server started: http://localhost:' + app.get('port') + '/');
