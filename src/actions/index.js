a8a6e037fe096487a9960cef91f7ef6eba00188c
Yuri Vaz <yurigava@gmail.com>
2017-04-30 02:43:56 -0300

    Started using Redux
    Change-Id: I912afa16f8ea25e62d7a76853c94f98fb456ff23

---
+++ src/actions/index.js
@@ -0,0 +1,76 @@
+import axios from 'axios'
+
+const serverUrl = 'http://192.168.0.69:8081';
+
+export const changeRole = (role) => {
+  return {
+    type: 'CHANGE_ROLE',
+    role
+  }
+}
+
+export const checkLogout = (link) => {
+  return (dispatch) => {
+    if (link !== 'Logout') {
+      dispatch(toggleDrawerState());
+    }
+    else {
+      dispatch(toggleDrawerState());
+      dispatch(setLoading(true));
+      return axios.get(serverUrl + '/logout', {withCredentials:true})
+      .then((response) => {
+        dispatch(setLoading(false));
+      })
+      .catch((error) => {
+        dispatch(setLoading(false));
+      });
+    }
+  }
+}
+
+export const setDrawerState = (drawerOpen) => {
+  return {
+    type: 'SET_DRAWER',
+    drawerOpen
+  }
+}
+
+export const setLoading = (isLoading) => {
+  return {
+    type: 'SET_LOADING',
+    isLoading
+  }
+}
+
+export const setLoginStatus = (success) => {
+  return {
+    type: 'SET_LOGIN_STATUS',
+    success
+  }
+}
+
+export const submitLogin = (login, password, push) => {
+  return (dispatch, getState) => {
+    dispatch(setLoading(true));
+    return axios.post(serverUrl + "/login", {
+      login: login,
+      password: password
+    }, {withCredentials:true})
+    .then((response) => {
+      dispatch(setLoading(false));
+      dispatch(setLoginStatus(true));
+      dispatch(changeRole(response.data))
+      push('/'+getState().appUi.linksById[getState().appUi.visibleLinks[0]].link)
+    })
+    .catch((error) => {
+      dispatch(setLoading(false));
+      dispatch(setLoginStatus(false));
+    });
+  }
+}
+
+export const toggleDrawerState = () => {
+  return {
+    type: 'TOGGLE_DRAWER'
+  }
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,45 +1,69 @@
 import React from 'react';
 import axios from 'axios';
 import ReactDOM from 'react-dom';
-import App from './components/App.jsx';
+import appContainer from './containers/appContainer.jsx';
+import loginContainer from './containers/loginContainer.jsx'
 import EquipTable from './components/EquipTable.jsx';
-import InputAuthentication from './components/InputAuthentication.jsx'
 import { Router, Route, hashHistory, IndexRedirect } from 'react-router';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunkMiddleware from 'redux-thunk'
+import almoxApp from './reducers/index.jsx'
+import { changeRole } from './actions/index.js'
 
-const url = "http://192.168.0.69:8081";
+const url = 'http://192.168.0.69:8081';
+let store = createStore(almoxApp, applyMiddleware(thunkMiddleware));
 
 main();
 
-function verifyPermission(nextState, replace, callback)
+function logUserOut(nextState, replace)
 {
-  axios.get(url + nextState.location.pathname, {withCredentials:true})
-  .then((response) => {
-    if (response.data !== 'ok') {
-      replace({ nextPathname: nextState.location.pathname }, '/login')
-    }
-    callback();
-  })
-  .catch((error) => {
-    replace({ nextPathname: nextState.location.pathname }, '/login')
-    callback();
-  });
+    store.dispatch(changeRole('loggedOut'));
+    replace({ nextPathname: nextState.location.pathname }, '/login', nextState.location.query);
 }
 
-function verifyIfLogged(nextState, replace, callback)
+function verifyPermission(nextState, replace)
 {
-  axios.get(url + '/getRole', {withCredentials:true})
-  .then((response) => {
-    //Lógica pra página inicial de cada papel
-    if (response.data === 'almoxarife') {
-      //replace({ nextPathname: nextState.location.pathname }, '/equips', nextState.location.query)
-    }
+  let { login } = store.getState();
+  let { appUi } = store.getState();
+  let linkIndex = appUi.linksById.findIndex(l => '/'+l.link === nextState.location.pathname);
+  if (appUi.visibleLinks.includes(linkIndex)) {
+  }
+  else {
+    //Manda para página de notAllowed
+    replace({ nextPathname: nextState.location.pathname }, '/login', nextState.location.query);
+  }
+}
+
+function verifyLoggedState(nextState, replace, callback)
+{
+  let { login } = store.getState();
+  let { appUi } = store.getState();
+  if (!login || !login.userRole) {
+    axios.get(url+'/getRole', {withCredentials:true})
+    .then((response) => {
+      store.dispatch(changeRole(response.data));
+      //Lógica pra página inicial de cada papel
+      //replace({ nextPathname: nextState.location.pathname }, '/'+appUi.linksById[appUi.visibleLinks[0]], nextState.location.query)
+      console.log("Async response: " + response.data)
+      callback();
+    })
+    .catch((error) => {
+      store.dispatch(changeRole('loggedOut'));
+      console.log("Async response: error")
+      callback();
+    });
+  }
+  else if (login.userRole !== 'loggedOut') {
+    console.log("User logged in as: " + login.userRole)
     callback();
-  })
-  .catch((error) => {
+  }
+  else {
+    console.log("User LoggedOut")
     callback();
-  });
+  }
 }
 
 function main() {
@@ -48,13 +72,16 @@ function main() {
   document.body.appendChild(app);
   ReactDOM.render(
     <MuiThemeProvider>
-      <Router history={hashHistory}>
-        <Route path="/" component={App} url={url}>
-          <IndexRedirect to="/login" />
-          <Route path="/login" component={InputAuthentication} url={url} onEnter={verifyIfLogged}/>
-          <Route path="/equips" component={EquipTable} url={url} onEnter={verifyPermission}/>
-        </Route>
-      </Router>
+      <Provider store={store}>
+        <Router history={hashHistory}>
+          <Route path="/" component={appContainer}>
+            <IndexRedirect to="/login" />
+            <Route path="/logout" onEnter={logUserOut}/>
+            <Route path="/login" component={loginContainer} onEnter={verifyLoggedState}/>
+            <Route path="/equips" component={EquipTable} url={url} onEnter={verifyPermission}/>
+          </Route>
+        </Router>
+      </Provider>
     </MuiThemeProvider>
     , app);
 }
