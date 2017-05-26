eeed78bcc57d3a1ce4a54889c806c4c2125e6855
Yuri Vaz <yurigava@gmail.com>
2017-05-25 23:08:56 -0300

    Created relational Tables for DB, started using ORM
    Change-Id: I964276dd751fec955c8a87b909a0e64577c83bc5

---
+++ src/models/ormModels.js
@@ -0,0 +1,146 @@
+module.exports = function(db, cb) {
+  var EquipamentosMonitorados = db.define("EquipamentosMonitorados", {
+    patrimonio: {
+      type: 'number',
+      size: 32,
+      required: true,
+      unique: true,
+      key: true
+    }
+  }, {autoFetchLimit: 2});
+
+  var Estados = db.define("Estados", {
+    id_estado: {
+      type: 'number',
+      size: 8,
+      required: true,
+      unique: true,
+      key: true
+    },
+    estado: {
+      type: 'text',
+      size: 30,
+      required: true
+    }
+  });
+
+  var EstadosReq = db.define("EstadosReq", {
+    id_estadosReq: {
+      type: 'number',
+      size: 8,
+      required: true,
+      unique: true,
+      key: true
+    },
+    estadoReq: {
+      type: 'text',
+      size: 30,
+      required: true
+    }
+  });
+
+  var HistoricoEquipamentos = db.define("HistoricoEquipamentos", {
+    id_evento: {
+      type: 'number',
+      size: 32,
+      required: true,
+      unique: true,
+      key: true
+    },
+    usuario: {
+      type: 'text',
+      size: 45,
+      required: true
+    }
+  });
+
+  var Requisicoes = db.define("Requisicoes", {
+    id_evento: {
+      type: 'number',
+      size: 32,
+      required: true,
+      unique: true,
+      key: true
+    },
+    materia: {
+      type: 'text',
+      size: 45,
+      required: false
+    },
+    usuario: {
+      type: 'text',
+      size: 45,
+      required: true
+    },
+    timestamp: {
+      type: 'date',
+      size: 45,
+      time: true,
+      required: true
+    }
+  });
+
+  var Familias = db.define("Familias", {
+    id_familia: {
+      type: 'number',
+      size: 32,
+      required: true,
+      unique: true,
+      key: true
+    },
+    familia: {
+      type: 'text',
+      size: 50,
+      required: true
+    }
+  });
+
+  var Tipos = db.define("Tipos", {
+    id_tipo: {
+      type: 'number',
+      size: 32,
+      required: true,
+      unique: true,
+      key: true
+    },
+    tipo: {
+      type: 'text',
+      size: 50,
+      required: true
+    }
+  });
+
+  EquipamentosMonitorados.hasOne("Estado", Estados, {
+    required: true,
+    field: "Estados_id_estado",
+    autoFetch: true
+  });
+
+  EquipamentosMonitorados.hasOne("Tipo", Tipos, {
+    required: true,
+    field: "Tipos_id_tipo",
+    autoFetch: true
+  });
+
+  HistoricoEquipamentos.hasOne("EquipamentoMonitorado", EquipamentosMonitorados, {
+    required: true,
+    field: "EquipamentosMonitorados_patrimonio",
+  });
+
+  HistoricoEquipamentos.hasOne("Requisicao", Requisicoes, {
+    required: true,
+    field: "Requisicoes_id_evento",
+  });
+
+  Requisicoes.hasOne("EstadoReq", EstadosReq, {
+    required: true,
+    field: "EstadosReq_id_estadosReq",
+  });
+
+  Tipos.hasOne("Familia", Familias, {
+    required: true,
+    field: "Familias_id_familia",
+    autoFetch: true
+  });
+  return cb();
+};
