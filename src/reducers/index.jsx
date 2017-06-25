62924763845b1e6cbd0e34f0d6dcf46acb3cb7cb
Yuri Vaz <yurigava@gmail.com>
2017-06-25 03:54:05 -0300

    Created page to add equipments
    - Splitted Actions
    - Changed color of Floating Texts
    - Created typeSelector component and containers
    
    Change-Id: Ia65100960b6ec93421bf6e385d6c71396bed1a9e

--- src/reducers/index.jsx
+++ src/reducers/index.jsx
@@ -1,11 +1,15 @@
 import { combineReducers } from 'redux'
 import login from './login.jsx'
 import appUi from './appUi.jsx'
+import addEquip from './addEquip.jsx'
+import equipTypeSelect from './equipTypeSelect.jsx'
 //import visibilityFilter from './visibilityFilter'
 
 const almoxApp = combineReducers({
   login,
-  appUi
+  appUi,
+  addEquip,
+  equipTypeSelect,
 })
 
 export default almoxApp
