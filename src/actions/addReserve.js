8718659923847d26879a32d73638365930b891c9
Ramon Felipe Pereira <ramon@alunos.utfpr.edu.br>
2017-07-12 00:30:46 -0300

    Create Page to Add Reserves merged
    --Receiving quantity of server OK, but error in axios.get with family
    --Merged with "Created page to lend Equipments to Students"
    --Send "requisicao" and "EquipamentosRequisicao" completed, without error handling
    
    Change-Id: I090e2587d6a9762141cc901e8ccaf9813793996b

---
+++ src/actions/addReserve.js
@@ -0,0 +1,61 @@
+export const setErrorDescription = (equipNumber, errorCode) => {
+  return {
+    type: 'SET_RESERVE_ERROR_DESCRIPTION',
+    equipNumber,
+    errorCode
+  }
+}
+
+export const setMissingFieldsError = (isMissingTipo, isMissingFamilia) => {
+  return {
+    type: 'SET_MISSING_FIELDS_ERROR_RESERVE',
+    isMissingTipo,
+    isMissingFamilia
+  }
+}
+
+export const insertReserve = (serverUrl, date, materia, equips) => {
+  return {
+    type: 'INSERT_RESERVE',
+    serverUrl,
+    date,
+    materia,
+    equips
+  }
+}
+
+export const quantidadeReserve = (serverUrl, familia) => {
+  return {
+    type: 'QUANTIDADE_RESERVE',
+    serverUrl,
+    familia
+  }
+}
+
+export const setQuantidade = (quantidade) => {
+  return {
+    type: 'SET_QUANTIDADE',
+    quantidade
+  }
+}
+
+export const setSelectedTipo = (tipo) => {
+  return {
+    type: 'ADD_RESERVE_SET_SELECTED_TIPO',
+    tipo
+  }
+}
+
+export const setSelectedFamilia = (familia) => {
+  return {
+    type: 'ADD_RESERVE_SET_SELECTED_FAMILIA',
+    familia
+  }
+}
+
+export const setinfoNumber = (infoNumber) => {
+  return {
+    type: 'ADD_RESERVE_SET_INFO_NUMBER',
+    infoNumber
+  }
+}
---
+++ src/containers/AddReserveContainer.jsx
@@ -0,0 +1,67 @@
+import { connect } from 'react-redux'
+import AddReserve from '../components/AddReserve.jsx'
+import {
+  setSelectedTipo,
+  setSelectedFamilia,
+  insertReserve,
+  quantidadeReserve,
+  setErrorDescription,
+  setMissingFieldsError,
+  setinfoNumber,
+} from '../actions/addReserve.js'
+import {
+  setSubmissionMessage,
+} from '../actions/appUi.js'
+import { serverUrl } from '../main.jsx'
+
+const mapStateToProps = (state) => {
+  return {
+    isInputDisabled: state.appUi.isInputDisabled,
+    submissionMessage: state.appUi.submissionMessage,
+    isDataSubmitted: state.appUi.isDataSubmitted,
+    isMissingTipo: state.addReserve.isMissingTipo,
+    isMissingFamilia: state.addReserve.isMissingFamilia,
+    tipo: state.addReserve.selectedTipo,
+    familia: state.addReserve.selectedFamilia,
+    errorCauseEquipNumber: state.addReserve.errorCauseEquipNumber,
+    errorCode: state.addReserve.errorCode,
+    infoNumber: state.addReserve.infoNumber,
+    quantidade: state.addReserve.quantidade,
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    insertReserve: (date, materia, equips) => {
+      dispatch(insertReserve(serverUrl, date, materia, equips))
+    },
+    quantidadeReserve: (familia) => {
+      dispatch(quantidadeReserve(serverUrl, familia))
+    },
+    clearSubmissionMessage: () => {
+      dispatch(setSubmissionMessage(""))
+    },
+    clearEquipNumberError: () => {
+      dispatch(setErrorDescription("", ""))
+    },
+    clearMissingFieldsError: () => {
+      dispatch(setMissingFieldsError(false, false))
+    },
+    setSelectedTipo: (name, tipo) => {
+      dispatch(setSelectedTipo(tipo))
+    },
+    setSelectedFamilia: (name, familia) => {
+      dispatch(setSelectedFamilia(familia))
+    },
+    setinfoNumber: (infoNumber) => {
+      dispatch(setinfoNumber(infoNumber))
+    }
+  }
+}
+
+const AddReserveContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(AddReserve)
+
+export default AddReserveContainer
