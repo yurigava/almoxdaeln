25427bc6249e93c7ce458e36cda7188d260cb9c0
Ramon Felipe Pereira <ramon@alunos.utfpr.edu.br>
2017-07-12 00:30:46 -0300

    Create Page to Add Reserves merged
    --Merged with actual projet
    --No sending INSERT_RESERVE to server.js
    --Error in update timeReserve
    --Send package with informations in progress
    
    Change-Id: I090e2587d6a9762141cc901e8ccaf9813793996b

---
+++ src/actions/addReserve.js
@@ -0,0 +1,46 @@
+export const setErrorDescription = (equipNumber, errorCode) => {
+  return {
+    type: 'SET_RESERVE_ERROR_DESCRIPTION',
+    equipNumber,
+    errorCode
+  }
+}
+
+export const setMissingFieldsError = (isMissingTipo, isMissingFamilia) => {
+  return {
+    type: 'SET_MISSING_FIELDS_ERROR_RESERVE',
+    isMissingTipo,
+    isMissingFamilia
+  }
+}
+
+export const insertReserve = (serverUrl, patrimonios, id_tipo, date) => {
+  return {
+    type: 'INSERT_RESERVE',
+    serverUrl,
+    patrimonios,
+    id_tipo,
+    date,
+  }
+}
+
+export const setSelectedTipo = (tipo) => {
+  return {
+    type: 'ADD_RESERVE_SET_SELECTED_TIPO',
+    tipo
+  }
+}
+
+export const setSelectedFamilia = (familia) => {
+  return {
+    type: 'ADD_RESERVE_SET_SELECTED_FAMILIA',
+    familia
+  }
+}
+
+export const setinfoNumber = (infoNumber) => {
+  return {
+    type: 'ADD_RESERVE_SET_INFO_NUMBER',
+    infoNumber
+  }
+}
