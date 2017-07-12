f15224c3073662aec24dfe458ea5c1e144bf60df
Ramon Felipe Pereira <ramon@alunos.utfpr.edu.br>
2017-07-12 00:30:46 -0300

    Create Page to Add Reserves
    --Decrease quantity of same family of type null in progress with error when select in order: type not null, type null, ERROR!
    --Tests at mobile will pass by props
    --Started transition to props instead of states
    
    Change-Id: I090e2587d6a9762141cc901e8ccaf9813793996b

---
+++ src/actions/addReserve.js
@@ -0,0 +1,73 @@
+export const insertReserve = (serverUrl, usuario, date, turno, materia, equips) => {
+  return {
+    type: 'INSERT_RESERVE',
+    serverUrl,
+    usuario,
+    date,
+    turno,
+    materia,
+    equips
+  }
+}
+
+export const quantidadeReserve = (serverUrl, familia, tipo, name, date, turno) => {
+  return {
+    type: 'QUANTIDADE_RESERVE',
+    serverUrl,
+    familia,
+    tipo,
+    name,
+    date,
+    turno
+  }
+}
+
+export const setQuantidade = (index, quantidade) => {
+  return {
+    type: 'RESERVE_SET_QUANTIDADE',
+    index,
+    quantidade
+  }
+}
+
+export const removeEquip = (index) => {
+  return {
+    type: 'RESERVE_REMOVE_EQUIP',
+    index
+  }
+}
+
+export const addEquip = () => {
+  return {
+    type: 'RESERVE_REMOVE_EQUIP'
+  }
+}
+
+export const clearEquips = () => {
+  return {
+    type: 'RESERVE_CLEAR_EQUIPS'
+  }
+}
+
+export const setSelectedTipo = (index, tipo) => {
+  return {
+    type: 'RESERVE_SET_SELECTED_TIPO',
+    index,
+    tipo
+  }
+}
+
+export const setSelectedFamilia = (index, familia) => {
+  return {
+    type: 'RESERVE_SET_SELECTED_FAMILIA',
+    index,
+    familia
+  }
+}
+
+export const setinfoNumber = (infoNumber) => {
+  return {
+    type: 'RESERVE_SET_INFO_NUMBER',
+    infoNumber
+  }
+}
