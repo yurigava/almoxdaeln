4a7b95755b5723cd07b5b6f527cf38ca1963bdb7
Ramon Felipe Pereira <ramon@alunos.utfpr.edu.br>
2017-07-12 00:30:46 -0300

    Create Page to Add Reserves
    --Add family and type loop in progress
    --Send package with informations in progress
    
    Change-Id: I090e2587d6a9762141cc901e8ccaf9813793996b

---
+++ src/actions/AddReserve.js
@@ -0,0 +1,46 @@
+export const setErrorDescription = (equipNumber, errorCode) => {
+  return {
+    type: 'SET_RESERVE_EQUIP_ERROR_DESCRIPTION',
+    equipNumber,
+    errorCode
+  }
+}
+
+export const setMissingFieldsError = (isMissingTipo, isMissingFamilia) => {
+  return {
+    type: 'SET_MISSING_FIELDS_ERROR',
+    isMissingTipo,
+    isMissingFamilia
+  }
+}
+
+export const insertEquips = (serverUrl, patrimonios, id_tipo, date) => {
+  return {
+    type: 'INSERT_RESERVE',
+    serverUrl,
+    patrimonios,
+    id_tipo,
+    date,
+  }
+}
+
+export const setSelectedTipo = (tipo) => {
+  return {
+    type: 'ADD_EQUIP_SET_SELECTED_TIPO',
+    tipo
+  }
+}
+
+export const setSelectedFamilia = (familia) => {
+  return {
+    type: 'ADD_EQUIP_SET_SELECTED_FAMILIA',
+    familia
+  }
+}
+
+export const setInfoText = (infoText) => {
+  return {
+    type: 'ADD_EQUIP_SET_INFO_TEXT',
+    infoText
+  }
+}
