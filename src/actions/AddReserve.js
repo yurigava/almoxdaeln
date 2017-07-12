a30477cf481f3fd3c5f67d755f656e1c7576475b
Ramon Felipe Pereira <ramon@alunos.utfpr.edu.br>
2017-07-12 00:30:46 -0300

    Create Page to Add Reserves
    Change-Id: I090e2587d6a9762141cc901e8ccaf9813793996b

---
+++ src/actions/AddReserve.js
@@ -0,0 +1,52 @@
+export const setSelectedTipo = (tipo) => {
+  return {
+    type: 'ADD_EQUIP_SET_SELECTED_TIPO',
+    tipo
+  }
+}
+
+export const setSelectedFamilia = (familia) => {
+  return {
+    type: 'ADD_EQUIP_SET_SELECTED_FAMILIA',
+    familia
+  }
+}
+
+export const insertEquips = (serverUrl, patrimonios, id_tipo) => {
+  return {
+    type: 'INSERT_EQUIPS',
+    serverUrl,
+    patrimonios,
+    id_tipo,
+  }
+}
+
+export const setErrorDescription = (equipNumber, errorCode) => {
+  return {
+    type: 'SET_INSERT_ERROR_DESCRIPTION',
+    equipNumber,
+    errorCode
+  }
+}
+
+export const setMissingFieldsError = (isMissingTipo, isMissingFamilia) => {
+  return {
+    type: 'SET_MISSING_FIELDS_ERROR',
+    isMissingTipo,
+    isMissingFamilia
+  }
+}
+
+export const setInfoText = (infoText) => {
+  return {
+    type: 'ADD_EQUIP_SET_INFO_TEXT',
+    infoText
+  }
+}
+
+export const setDataSubmitted = (submitted) => {
+  return {
+    type: 'SET_DATA_SUBMITTED',
+    submitted
+  }
+}
