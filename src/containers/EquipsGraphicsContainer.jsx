8c8e9a40a9356c5e0b29f6866a339a45fe858e7e
Ramon Felipe Pereira <ramon@alunos.utfpr.edu.br>
2017-08-23 20:26:48 -0300

    Added Page EquipGrafhics
    --Added button to change width of bar graph OK but it is necessary minimaze and maximaze the brownser
    --Added request to server OK but the logic it not work for each day
    
    Change-Id: I2c0a44e039723fb6ebc2bc7fe0f714ff77680b90

---
+++ src/containers/EquipsGraphicsContainer.jsx
@@ -0,0 +1,68 @@
+import { connect } from 'react-redux'
+import EquipsGraphics from '../components/EquipsGraphics.jsx'
+import {
+  setMissingFieldsError,
+  setSelectedTipo,
+  setSelectedFamilia,
+  setInfoNumber,
+  raiseLowerWidth,
+  quantidadeEquipsGraph,
+} from '../actions/equipsGraphics.js'
+import {
+  setSubmissionMessage,
+} from '../actions/appUi.js'
+import { serverUrl } from '../main.jsx'
+
+const mapStateToProps = (state) => {
+  return {
+    tipo: state.equipsGraphics.selectedTipo,
+    familia: state.equipsGraphics.selectedFamilia,
+    isMissingFamilia: state.equipsGraphics.isMissingFamilia,
+    isMissingTipo: state.equipsGraphics.isMissingTipo,
+    submissionMessage: state.appUi.submissionMessage,
+    infoNumber: state.equipsGraphics.infoNumber,
+    width: state.equipsGraphics.width,
+    quantidadeM: state.equipsGraphics.quantidadeM,
+    quantidadeT: state.equipsGraphics.quantidadeT,
+    quantidadeN: state.equipsGraphics.quantidadeN,
+    referencia: state.equipsGraphics.referencia,
+    isInputDisabled: state.appUi.isInputDisabled,
+    //errorCode: state.equipsGraphics.errorCode,
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    clearMissingFieldsError: () => {
+      dispatch(setMissingFieldsError(false, false))
+    },
+    setSelectedTipo: (name, tipo) => {
+      dispatch(setSelectedTipo(tipo))
+    },
+    setSelectedFamilia: (name, familia) => {
+      dispatch(setSelectedFamilia(familia))
+    },
+    setInfoNumber: (infoNumber) => {
+      dispatch(setInfoNumber(infoNumber))
+    },
+    raiseLowerWidth: (widthActual, width) => {
+      dispatch(raiseLowerWidth(widthActual, width))
+    },
+    quantidadeEquipsGraph: (familia, tipo, dataInicial, dataFinal) => {
+      dispatch(quantidadeEquipsGraph(serverUrl, familia, tipo, dataInicial, dataFinal))
+    },
+    getTipos: () => {
+      dispatch(getTipos(serverUrl))
+    },
+    getFamilias: () => {
+      dispatch(getFamilias(serverUrl))
+    }
+  }
+}
+
+const EquipsGraphicsContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(EquipsGraphics)
+
+export default EquipsGraphicsContainer
---
+++ src/reducers/equipsGraphics.jsx
@@ -0,0 +1,70 @@
+import update from 'immutability-helper';
+
+const initialState =
+{
+  selectedTipo: null,
+  selectedFamilia: null,
+  isMissingTipo: false,
+  isMissingFamilia: false,
+  infoNumber: 0,
+  width: 75,
+  quantidadeM: 0,
+  quantidadeT: 0,
+  quantidadeN: 0,
+  referencia: 0,
+}
+
+const equipsGraphics = (state = initialState, action) => {
+  switch (action.type) {
+    case 'SET_MISSING_FIELDS_ERROR_EQUIPGRAPH':
+      return (
+        update(state, {
+          isMissingFamilia: {$set: action.isMissingFamilia},
+          isMissingTipo: {$set: action.isMissingTipo}
+        })
+      )
+
+    case 'EQUIPGRAPH_SET_SELECTED_FAMILIA':
+      return (
+          update(state, {
+            selectedFamilia: {$set: action.familia}
+          })
+      )
+
+    case 'EQUIPGRAPH_SET_SELECTED_TIPO':
+      return (
+          update(state, {
+            selectedTipo: {$set: action.tipo}
+          })
+      )
+          
+    case 'EQUIPGRAPH_SET_INFO_NUMBER':
+      return (
+          update(state, {
+            infoNumber: {$set: action.infoNumber}
+          })
+      )
+
+    case 'RAISE_LOWER_WIDTH_EQUIPGRAPH':
+      return (
+          update(state, {
+            width: {$set: (action.widthActual + action.width) }
+          })
+      )
+
+    case 'SET_QUANTIDADE_EQUIPGRAPH':
+      return (
+        update(state, {
+          quantidadeM: {$set: action.quantidadeM},
+          quantidadeT: {$set: action.quantidadeT},
+          quantidadeN: {$set: action.quantidadeN},
+          referencia: {$set: action.referencia}
+        })
+      )
+
+    default:
+      return state
+  }
+}
+
+export default equipsGraphics
