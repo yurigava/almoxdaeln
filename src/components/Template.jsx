75b9cd1e6647c5e4039736f753898360bcb26098
Yuri Vaz <yurigava@gmail.com>
2017-09-24 11:58:36 -0300

    Added page to prepare reserves, bug fixes, added role control
    - Changed main.jsx and App.jsx to support subroutes
    - Fixed missing propType imports
    - Added spinner before page loads
    - Added user to login and getRole Response
    - Added getting user role from DB
    - Updated material UI
    - Fixed login Error when server is offline
    
    Change-Id: Ied1baf8ef9f28ebf97d30d462618e4e9b9d585bd

--- src/components/Template.jsx
+++ src/components/Template.jsx
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import SelectField from 'material-ui/SelectField';
 import TextField from 'material-ui/TextField';
 import MenuItem from 'material-ui/MenuItem';
