d3d2fe935f612fd48d92cc3c16f3ac234366cb76
Yuri Vaz <yurigava@gmail.com>
2017-09-24 11:58:36 -0300

    Added page to prepare reserves, bug fixes
    - Changed main.jsx and App.jsx to support subroutes
    - Fixed missing propType imports
    - Added spinner before page loads
    
    Change-Id: Ied1baf8ef9f28ebf97d30d462618e4e9b9d585bd

---
+++ src/containers/ReadEquipsReserveContainer.jsx
@@ -0,0 +1,85 @@
+import { connect } from 'react-redux'
+import ReadEquipsReserve from '../components/ReadEquipsReserve.jsx'
+import {
+  setInfoNumber,
+  setBarcodeValue,
+  removeBarcode,
+  addBarcode,
+  incrementMissingReserve,
+  decrementMissingReserve,
+  setBarcodeErrorText,
+  getEquipTipo,
+  clearBarcodes,
+  resetMissingReserve,
+  setBarcodeTipo,
+  insertStudentLend
+} from '../actions/prepareReserve.js'
+import { getTipos, getFamilias } from '../actions/equipTypeSelect.js'
+import { serverUrl } from '../main.jsx'
+
+const mapStateToProps = (state) => {
+  return {
+    reserveEquips: state.prepareReserve.reserveEquips,
+    barCodes: state.prepareReserve.barCodes,
+    tipos: state.equipTypeSelect.tipos,
+    familias: state.equipTypeSelect.familias,
+    usuario: state.login.usuario
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setInfoNumber: (infoNumber) => {
+      dispatch(setInfoNumber(infoNumber))
+    },
+    setBarcodeValue: (index, value) => {
+      dispatch(setBarcodeValue(index, value))
+    },
+    removeBarcode: (index) => {
+      dispatch(removeBarcode(index))
+    },
+    addBarcode: () => {
+      dispatch(addBarcode())
+    },
+    clearBarcodes: () => {
+      dispatch(clearBarcodes())
+    },
+    incrementMissingReserve: (index) => {
+      dispatch(incrementMissingReserve(index))
+    },
+    decrementMissingReserve: (index) => {
+      dispatch(decrementMissingReserve(index))
+    },
+    resetMissingReserve: (index) => {
+      dispatch(resetMissingReserve(index))
+    },
+    setBarcodeErrorText: (index, errorText) => {
+      dispatch(setBarcodeErrorText(index, errorText))
+    },
+    clearBarcodeTipo: (index) => {
+      dispatch(setBarcodeTipo(index, null))
+    },
+    setBarcodeTipo: (index, tipo) => {
+      dispatch(setBarcodeTipo(index, tipo))
+    },
+    getEquipTipo: (index, barCode) => {
+      dispatch(getEquipTipo(serverUrl, index, barCode))
+    },
+    getTipos: () => {
+      dispatch(getTipos(serverUrl))
+    },
+    getFamilias: () => {
+      dispatch(getFamilias(serverUrl))
+    },
+    insertStudentLend: (usuario, patrimonios) => {
+      dispatch(insertStudentLend(serverUrl, usuario, patrimonios, true))
+    },
+  }
+}
+
+const ReadEquipsReserveContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ReadEquipsReserve)
+
+export default ReadEquipsReserveContainer
