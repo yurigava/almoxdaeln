bfac12b1e2d04bc641f6532c5b64d69ff915d27d
Ramon Felipe Pereira <ramon@alunos.utfpr.edu.br>
2017-10-31 23:09:22 -0200

    Added page's to Manager Users
    --Manager Users worked
    
    Change-Id: I49fd798575a6e353e5913e6b8b8e9028815fb841

---
+++ src/sagas/managerUser.jsx
@@ -0,0 +1,40 @@
+import { call, put, takeEvery, takeLatest } from 'redux-saga/effects'
+import axios from 'axios'
+
+function *getUser(action) {
+  yield put({ type: 'SET_LOADING', isLoading: true });
+  try {
+    const response = yield call(
+      axios.post,
+      action.serverUrl + '/api/getUser',
+      {
+        usuario: action.usuario,
+      },
+      {withCredentials:true}
+    );
+    if(response.data.code === "SUCCESS") {
+      //yield put({ type: 'RESERVE_SET_LAST_REQ', equipamentos: response.data.equipamentos , requisicoes: response.data.requisicoes });
+      yield put({ type: 'MANAGER_USER_SET_ALL_USER', users: response.data.users });
+    }
+    else {
+      //yield put({ type: 'SET_DATA_SUBMITTED', submitted: false });
+      yield put({ type: 'SET_SUBMISSION_MESSAGE', message: "Erro nos usuários"});
+      yield put({ type: 'MANAGER_USER_SET_ALL_USER', users: response.data.users });
+    }
+    //if(response.data.code === "SUCCESS") {
+      
+      //yield put({ type: 'SET_LOADING', isLoading: false });
+    //}
+  }
+  catch (e) {
+    //yield put({ type: 'SET_DATA_SUBMITTED', submitted: false });
+    yield put({ type: 'SET_SUBMISSION_MESSAGE', message: "Falha ao comunicar com o servidor"});
+  }
+  yield put({ type: 'SET_LOADING', isLoading: false });
+}
+
+function *managerUserSagas() {
+    yield takeEvery('MANAGER_USER_GET_USERS', getUser);
+}
+
+export default managerUserSagas;
